ca874d94415f37d619dc5d2bdc2bdcf9a797f6a1
Cristopher <cristoperesc@gmail.com>
2024-04-07 13:53:32 -0500

    title-fix

--- app/Components/Buttons/PreviousSection.tsx
+++ app/Components/Buttons/PreviousSection.tsx
@@ -6,7 +6,7 @@ export default function PreviousSection({ func }: {func: MouseEventHandler}) {
     <button
       title="Home"
       type="button"
-      className="w-[50px] h-[50px] absolute left-5 md:w-[60px] md:h-[60px]"
+      className="w-[50px] h-[50px] absolute left-1 top-0 md:w-[60px] md:h-[60px]"
       onClick={func}
     >
       <BackSquare />
--- app/Components/Title.tsx
+++ app/Components/Title.tsx
@@ -1,6 +1,6 @@
 export default function Title({title}: {title: string}) {
   return (
-    <h1 className="text-3xl w-[200px] font-semibold text-center md:text-4xl md:w-[300px]">
+    <h1 className="text-3xl w-[200px] font-semibold text-center md:text-4xl md:w-[300px] mt-1">
       {title}
     </h1>
   )
