65e608e8014ab81be2f0fecb561b6d3e101ecb1a
cristoperesc <cristoperesc@gmail.com>
2024-05-29 01:41:55 +0000

    trig functions

--- app/Logic/useCalculator.ts
+++ app/Logic/useCalculator.ts
@@ -3,20 +3,80 @@ import { useState, ChangeEventHandler } from "react"
 export default function useCalculator() {
   const [value, setValue] = useState<string>("")
 
+  function replaceFromEnd(val: string, replaceVal: string, str = value): string {
+    const lastIndex = str.lastIndexOf(val);
+
+    if (lastIndex !== -1) return str.substring(0, lastIndex) + replaceVal;
+
+    return str;
+  }
+
+  function strWithoutOps(str: string) {
+    const operators = ["+", "-", "*", "/"];
+    const indexes = operators.map((operator: string) => value.lastIndexOf(operator));
+    const lastIndex = Math.max(...indexes);
+
+    if (lastIndex === -1) return str;
+
+    return str.slice(lastIndex);
+  }
+
+  function addTrigFunction(trigFunc: string, val = value, onSetVal = onSetValue) {
+    const lastVal = String(val[val.length - 1]);
+    const operationsRegex = /[+\-*\/]/g;
+    const numbersRegex = /[\d]/g;
+
+    if (value === "") {
+      onSetVal(trigFunc + "(");
+    } else if (operationsRegex.test(lastVal)) {
+      onSetVal(trigFunc + "(");
+    } else if (numbersRegex.test(lastVal)) {
+      onSetVal("*" + trigFunc + "(");
+    } else {
+      onSetVal("");
+    }
+  }
+
+  const sine = () => {
+    addTrigFunction("sin");
+  };
+
+  const cosine = () => {
+    addTrigFunction("cos");
+  };
+
+  const tangent = () => {
+    addTrigFunction("tan");
+  };
+
   const onSetValue = (e: string) => {
     setValue(value.concat(e))
+  }
+
+  const onEval = (expression: string) => {
+    expression = expression.replace(/sin/g, "Math.sin");
+    expression = expression.replace(/cos/g, "Math.cos");
+    expression = expression.replace(/tan/g, "Math.tan");
+
+    try {
+      setValue(String(eval(expression)));
+    } catch {
+      setValue("Syntax Error")
+    }
   }
 
   const onEqual = () => {
     try {
       setValue(String(eval(value)));
-    } catch (error) {
+    } catch {
       setValue("Syntax Error")
     }
   }
 
   const onDeleteLastElement = () => {
-    if (String(value).length > 0) {
+    if (value === "Syntax Error") {
+      setValue("");
+    } else if (String(value).length > 0) {
       const delLastEl = String(value).split("");
       delLastEl.pop();
       const newValue = delLastEl.join("");
@@ -49,6 +109,13 @@ export default function useCalculator() {
     onParentheses,
     value,
     setValue,
-    handleValue
+    handleValue,
+    onEval,
+    strWithoutOps,
+    replaceFromEnd,
+    sine,
+    cosine,
+    tangent
+
   }
 }
--- app/Pages/CalculatorPro.tsx
+++ app/Pages/CalculatorPro.tsx
@@ -5,74 +5,119 @@ import { MouseEventHandler } from "react";
 import PreviousSection from "../Components/Buttons/PreviousSection";
 
 export default function CalculatorPro({ handlePrevious }: { handlePrevious: MouseEventHandler }) {
-  const { value, setValue, onDeleteLastElement, onEqual, onParentheses, onSetValue, handleValue } = useCalculator();
+  const {
+    value,
+    setValue,
+    onDeleteLastElement,
+    onParentheses,
+    onSetValue,
+    handleValue,
+    replaceFromEnd,
+    strWithoutOps,
+    onEval,
+    sine,
+    cosine, 
+    tangent
 
-  const onSquare = () => {
-    const strArr = (value).split(/[()+\-/*\s]/);
-    if (strArr.includes("")) {
-      const clearArr = strArr.filter(num => num !== "")
-      console.log(clearArr)
-      const lastStr = Number(clearArr[clearArr.length - 1])
-      const square = String(lastStr * lastStr);
-      console.log(square)
-      onSetValue(square);
+  } = useCalculator();
+
+  /*
+  square
+  potentiation
+  sin, cos, tan
+  ln, log10
+  square root, roots
+  pi
+  fractions, mixed fractions
+  */
+
+  const turnValue = () => {
+  const regexGeneral = /[^\d()\-\+*/]+/g;
+  const regexNumeric = /[^\d]+/g;
+
+  //general values
+  const strGeneral = value.split(regexGeneral);
+  const lastStrGeneral = strGeneral[strGeneral.length - 1];
+  const finalStr = strWithoutOps(lastStrGeneral);
+
+  //numeric values
+  const strNumeric = value.split(regexNumeric);
+  const strTrimed = strNumeric.filter(num => num !== "");
+  const lastStrNumeric = strTrimed[strTrimed.length - 1];
+
+    if (!lastStrGeneral) {
+      onSetValue("-(");
+    } else if (finalStr === lastStrNumeric) {
+      setValue(replaceFromEnd(finalStr, `-(${lastStrNumeric})`));
+    } else if (finalStr === `-(${lastStrNumeric})`) {
+      setValue(replaceFromEnd(finalStr, lastStrNumeric));
+    } else if (finalStr === `-${lastStrNumeric}`) {
+      setValue(replaceFromEnd(finalStr, `+${lastStrNumeric}`));
+    } else if (finalStr === `(${lastStrNumeric}`) {
+      setValue(replaceFromEnd(finalStr, `-(${lastStrNumeric}`));
+    } else if (finalStr === `-(${lastStrNumeric}`) {
+      setValue(replaceFromEnd(finalStr, lastStrNumeric));
+    } else if (finalStr === `+${lastStrNumeric}`) {
+      setValue(replaceFromEnd(finalStr, `-${lastStrNumeric}`));
+    } else if (finalStr === `*${lastStrNumeric}`) {
+      setValue(replaceFromEnd(finalStr, `*-(${lastStrNumeric})`));
+    } else if (finalStr === `*-${lastStrNumeric}`) {
+      setValue(replaceFromEnd(finalStr, `*${lastStrNumeric}`));
+    } else if (finalStr === `/${lastStrNumeric}`) {
+      setValue(replaceFromEnd(finalStr, `/-(${lastStrNumeric})`));
+    } else if (finalStr === `/-${lastStrNumeric}`) {
+      setValue(replaceFromEnd(finalStr, `/${lastStrNumeric}`));
+    } else if (finalStr === `(${lastStrNumeric})`) {
+      setValue(replaceFromEnd(finalStr, `-(${lastStrNumeric})`));
+    } else {
+      onSetValue("");
     }
   }
 
-  const onPotentiation = () => {
 
-  }
+  const advancedButtons = [
+    { val: "SIN", func: () => sine() },
+    { val: "COS", func: () => cosine() },
+    { val: "TAN", func: () => tangent() },
+    { val: "**", func: () => onSetValue("**") },
+    { val: "**2", func: () => onSetValue("**2") },
+    { val: "ln", func: () => { } },
+    { val: "log10", func: () => { } },
+    { val: "root", func: () => { } },
+    { val: "rootP", func: () => { } },
+    { val: "n/n", func: () => { } },
+    { val: "n n/n", func: () => { } },
+  ]
 
-  const onNegative = () => {
-
-  }
-
-  const onSqrt = () => {
-
-  }
-
-  const onSine = () => {
-
-  }
-
-  const onCosine = () => {
-
-  }
-
-  const onTangent = () => {
-
-  }
-
-  const onPI = () => {
-
-  }
-
-  const onLogNatural = () => {
-
-  }
-
-  const onLogBaseTen = () => {
-
-  }
-
-  const onFraction = () => {
-
-  }
-
-  const onNumberMix = () => {
-
-  }
-
-  const onAns = () => {
-
-  }
+  const buttons = [
+    { val: "C", func: () => setValue("") },
+    { val: "Del", func: () => onDeleteLastElement() },
+    { val: "( )", func: () => onParentheses() },
+    { val: "÷", func: () => onSetValue("/") },
+    { val: "7", func: () => onSetValue("7") },
+    { val: "8", func: () => onSetValue("8") },
+    { val: "9", func: () => onSetValue("9") },
+    { val: "×", func: () => onSetValue("*") },
+    { val: "4", func: () => onSetValue("4") },
+    { val: "5", func: () => onSetValue("5") },
+    { val: "6", func: () => onSetValue("6") },
+    { val: "-", func: () => onSetValue("-") },
+    { val: "1", func: () => onSetValue("1") },
+    { val: "2", func: () => onSetValue("2") },
+    { val: "3", func: () => onSetValue("3") },
+    { val: "+", func: () => onSetValue("+") },
+    { val: "+/-", func: () => turnValue() },
+    { val: "0", func: () => onSetValue("0") },
+    { val: ".", func: () => onSetValue(".") },
+    { val: "=", func: () => onEval(value) }
+  ];
 
   return (
     <>
       <PreviousSection func={handlePrevious} />
-      <Title title="Pronto..." />
-      {/* <Title title="Calculadora Avanzada" />
-      <section className="text-xl font-semibold">
+      {/* <Title title="Pronto..." /> */}
+      <Title title="Calculadora Avanzada" />
+      <section className="text-xl mt-5 font-semibold">
         <form>
           <input
             title="display"
@@ -80,56 +125,29 @@ export default function CalculatorPro({ handlePrevious }: { handlePrevious: Mous
             value={value}
             onChange={handleValue}
             name="display"
-            className="w-[360px] h-20 rounded-md"
+            className="w-[450px] h-16 rounded-md"
             readOnly
           />
         </form>
-        <div className="flex flex-row gap-4">
-          <aside className="flex flex-col gap-2">
-            <button className="w-20 h-20 bg-white rounded-md">123</button>
-            <button className="w-20 h-20 bg-white rounded-md">f(x)</button>
-            <button className="w-20 h-20 bg-white rounded-md">ABC</button>
-          </aside>
-          <div className="grid grid-cols-4 grid-rows-4 gap-2">
-            <CalculatorProButton value="^(2)" func={() => onSetValue("**2")} />
-            <CalculatorProButton value="^(n)" func={() => onSetValue("**")} />
-            <CalculatorProButton value="√n" func={onSqrt} />
-            <CalculatorProButton value="sen" func={onSine} />
-            <CalculatorProButton value="cos" func={onCosine} />
-            <CalculatorProButton value="tg" func={onTangent} />
-            <CalculatorProButton value="π" func={onPI} />
-            <CalculatorProButton value="ln" func={onLogNatural} />
-            <CalculatorProButton value="log10" func={onLogBaseTen} />
-            <CalculatorProButton value="n/n" func={onFraction} />
-            <CalculatorProButton value="n n/n" func={onNumberMix} />
-            <CalculatorProButton value="ans" func={onAns} />
-            <CalculatorProButton value="," func={() => { onSetValue(",") }} />
-            <CalculatorProButton value="( )" func={() => onParentheses()} />
-            <CalculatorProButton value="clr" func={() => setValue("")} />
+        <div className="flex flex-row gap-4 mt-2">
+          <div className="grid grid-cols-3 grid-rows-4 gap-2 h-[80%]">
+            {/* buttons for advanced features */}
+            {advancedButtons.map((btn) => {
+              return (
+                <CalculatorProButton key={btn.val} value={btn.val} func={btn.func} />
+              )
+            })}
           </div>
           <div className="grid grid-cols-4 gap-2">
-            <CalculatorProButton value="7" func={() => onSetValue("7")} />
-            <CalculatorProButton value="8" func={() => onSetValue("8")} />
-            <CalculatorProButton value="9" func={() => onSetValue("9")} />
-            <CalculatorProButton value="×" func={() => onSetValue("*")} />
-            <CalculatorProButton value="4" func={() => onSetValue("4")} />
-            <CalculatorProButton value="5" func={() => onSetValue("5")} />
-            <CalculatorProButton value="6" func={() => onSetValue("6")} />
-            <CalculatorProButton value="÷" func={() => onSetValue("/")} />
-            <CalculatorProButton value="1" func={() => onSetValue("1")} />
-            <CalculatorProButton value="2" func={() => onSetValue("2")} />
-            <CalculatorProButton value="3" func={() => onSetValue("3")} />
-            <CalculatorProButton value="-" func={() => onSetValue("-")} />
-            <CalculatorProButton value="." func={() => onSetValue(".")} />
-            <CalculatorProButton value="0" func={() => onSetValue("0")} />
-            <CalculatorProButton value="=" func={() => onEqual()} />
-            <CalculatorProButton value="+" func={() => onSetValue("+")} />
+            {/* buttons for normal features */}
+            {buttons.map((btn) => {
+              return (
+                <CalculatorProButton key={btn.val} value={btn.val} func={btn.func} />
+              )
+            })}
           </div>
         </div>
       </section >
-      */}
     </>
   )
 }
-
-//source: https://www.geogebra.org/scientific?lang=es
